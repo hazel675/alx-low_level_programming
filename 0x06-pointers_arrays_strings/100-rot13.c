e9081e027cb66d82ee508acedbe799be5d63dabd
hazel675 <oge565kanma@gmail.com>
2023-01-03 09:24:26 -0800

    task 0

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,18 +6,18 @@
  *
  * Return: A pointer to the encoded string
  */
-char  *rot13(char *str)
+char *rot13(char *str)
 {
 	int indx1 = 0, indx2;
 	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
 			     'G', 'H', 'I', 'J', 'K', 'L',
-			     'M', 'N', 'O', 'P', 'Q', 'R',
-			     'S', 'T', 'U', 'V', 'W', 'X',
-			     'Y', 'Z', 'a', 'b', 'c', 'd',
-			     'e', 'f', 'g', 'h', 'i', 'j',
-			     'k', 'l', 'm', 'n', 'o', 'p',
-			     'q', 'r', 's', 't', 'u', 'v',
-			     'w', 'x', 'y', 'z'};
+		    	     'M', 'N', 'O', 'P', 'Q', 'R',
+		     	     'S', 'T', 'U', 'V', 'W', 'X',
+		     	     'Y', 'Z', 'a', 'b', 'c', 'd',
+		     	     'e', 'f', 'g', 'h', 'i', 'j',
+		     	     'k', 'l', 'm', 'n', 'o', 'p',
+		     	     'q', 'r', 's', 't', 'u', 'v',
+		      	     'w', 'x', 'y', 'z'};
 	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
 			     'T', 'U', 'V', 'W', 'X', 'Y',
 			     'Z', 'A', 'B', 'C', 'D', 'E',
@@ -26,7 +26,7 @@ char  *rot13(char *str)
 			     'r', 's', 't', 'u', 'v', 'w',
 			     'x', 'y', 'z', 'a', 'b', 'c',
 			     'd', 'e', 'f', 'g', 'h', 'i',
-			     'j', 'k', 'l', 'm'};
+			      'j', 'k', 'l', 'm'};
 	while (str[indx1])
 	{
 		for (indx2 = 0; indx2 < 52; indx2++)
@@ -43,3 +43,4 @@ char  *rot13(char *str)
 
 	return (str);
 }
+
