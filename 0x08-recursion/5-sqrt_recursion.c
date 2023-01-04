d71dabad1bc98b37aaa3136667bf6b01e7623efd
hazel675 <oge565kanma@gmail.com>
2023-01-04 10:29:31 -0800

    task 5 updated

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -20,27 +20,3 @@ int find_sq(int x, int y)
 	}
 	return (find_sq(x + 1, y));
 }
-
-/**
- * _sqrt_recursion - prints the natural square root of a number
- *
- * @n: number to get evaluated
- *
- * Return: the natural square root of @n, -1 if no natural root found
- */
-int _sqrt_recursion(int n)
-{
-	if (n < 0)
-	{
-		return (-1);
-	}
-	if (n == 0)
-	{
-		return (0);
-	}
-	if (n == 1)
-	{
-		return (1);
-	}
-	return (find_sq(2, n));
-}
