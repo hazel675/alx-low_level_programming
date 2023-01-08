d3d3f2f09cf1d564f02fd7f13e04cae73fbbc5c9
hazel675 <oge565kanma@gmail.com>
2023-01-08 05:28:56 -0800

    task 2 updated

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,9 +12,9 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0, i < argc; 1++)
+	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n", argv[1])
+		printf("%s\n", argv[1]);
 	}
 
 	return (0);
